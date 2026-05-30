6214ddf52ff1b2cd14ea1db84f20033870cda5e3
parampatel20 <bparampatel20@gmail.com>
2026-05-30 18:16:34 +0530

    Update categories.js

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -3,9 +3,9 @@ const router  = express.Router();
 const { addCategory, getCategories, updateCategory, deleteCategory } = require('../controllers/categoryController');
 const { protect, adminOnly } = require('../middleware/auth');
 
-router.get('/',      protect, getCategories);
-router.post('/',     protect, addCategory);
-router.put('/:id',   protect, adminOnly, updateCategory);
-router.delete('/:id',protect, adminOnly, deleteCategory);
+router.get('/',       protect, getCategories);
+router.post('/',      protect, adminOnly, addCategory);
+router.put('/:id',    protect, adminOnly, updateCategory);
+router.delete('/:id', protect, adminOnly, deleteCategory);
 
 module.exports = router;
